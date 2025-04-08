21acae6e489c53aba54187b4918e1ed96c45f6f2
andersonromeiro <andersonromeiro@gmail.com>
2025-04-08 15:46:35 -0300

    commit

--- concatenacao.js
+++ concatenacao.js
@@ -1,17 +1,3 @@
-/*
-   nome: caractere
-   numero: real
-
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc...
-   escreval ("Digite o nome:")
-   leia (nome)
-   escreval ("Digite a idade:")
-   leia (numero)
-   
-   escreval (nome, " : ", numero, " anos")
-*/
-
 var nome, numero;
 
 nome = prompt ("Digite o nome:")
--- estrutura-controle.js
+++ estrutura-controle.js
@@ -1,17 +1,3 @@
-/*escreval ("Digite Nome do Aluno")
-leia (nome)
-escreval ("Digite a primeira nota")
-leia (nota01)
-escreval ("Digite a segunda nota")
-leia (nota02)
-media:= (nota01 + nota02) / 2
-
-se media >= 50 entao
-   escreval ("Aprovado!", nome)
-senao
-   escreval ("Reprovado!", nome)
-fimse*/
-
 var nome, nota01, nota02;
 nome = prompt ("Digite Nome do Aluno:")
 nota01 = prompt ("Digite a primeira nota do aluno:")
